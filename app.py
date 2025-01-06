2cca6f848c16db3d4424c4a9820e02b574642353
Soumyaatanna <165031234+Soumyaatanna@users.noreply.github.com>
2025-01-06 23:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def optimize_route():
     origin = data['origin']
     destination = data['destination']
     
-    traffic_data = get_traffic_data('fYApAX0LcuZAte6jxs5rQuy2agdR7IEw', origin, destination)
-    route_data = get_route_data('AIzaSyCwH1bQGO_BBRAhOzi7l2a4rO6kdEPkKRY', origin, destination)
-    weather_data = get_weather_data('48c9a2fcec48916a2ac2be3d09aaa9ce3df16079', origin)
+    traffic_data = get_traffic_data('tomtom_api_key', origin, destination)
+    route_data = get_route_data('google_map_api_key', origin, destination)
+    weather_data = get_weather_data('api_key', origin)
     
     distance = route_data['routes'][0]['legs'][0]['distance']['value'] / 1000  # Convert to km
     emissions = calculate_emissions(distance, vehicle_details['fuel_efficiency'])
